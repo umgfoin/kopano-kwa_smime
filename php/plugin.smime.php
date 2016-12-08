1140cc7f7d1033b8525014b6157b5adfe3dba6b6
Jelle van der Waa <j.vanderwaa@kopano.com>
2016-12-08 13:11:20 +0100

    move getSenderAddress to S/MIME
    getSenderAddress is unused in WebApp and will therefore be removed, add
    code to handle the removal.

--- php/plugin.smime.php
+++ php/plugin.smime.php
@@ -193,7 +193,7 @@ class Pluginsmime extends Plugin {
 		// When downloading an email as eml, $GLOBALS['operations'] isn't set, so add a check so that downloading works
 		// If the certificate is already fetch from the GAB, skip checking the userStore.
 		if (!$fromGAB && isset($GLOBALS['operations'])) {
-			$senderAddressArray = $GLOBALS["operations"]->getSenderAddress($message);
+			$senderAddressArray = $this->getSenderAddress($message);
 			$senderAddressArray = $senderAddressArray['props'];
 			if($senderAddressArray['address_type'] === 'SMTP') {
 				$emailAddr = $senderAddressArray['email_address'];
@@ -1093,6 +1093,40 @@ class Pluginsmime extends Plugin {
 				)
 			)
 		));
+	}
+
+	/**
+	 * Get sender structure of the MAPI Message.
+	 *
+	 * @param mapimessage $mapiMessage  MAPI Message resource from which we need to get the sender.
+	 * @return array with properties
+	 */
+	function getSenderAddress($mapiMessage)
+	{
+		// WebApp will remove this method in the future (3.4.0)
+		if (!method_exists($GLOBALS['operations'], 'getSenderAddress')) {
+			$messageProps  = mapi_getprops($mapiMessage, array(PR_SENT_REPRESENTING_ENTRYID, PR_SENDER_ENTRYID));
+			$senderEntryID = isset($messageProps[PR_SENT_REPRESENTING_ENTRYID])? $messageProps[PR_SENT_REPRESENTING_ENTRYID] : $messageProps[PR_SENDER_ENTRYID];
+			$senderUser = mapi_ab_openentry($GLOBALS["mapisession"]->getAddressbook(), $senderEntryID);
+
+			if ($senderUser) {
+				$userprops = mapi_getprops($senderUser, array(PR_ADDRTYPE, PR_DISPLAY_NAME, PR_EMAIL_ADDRESS, PR_SMTP_ADDRESS, PR_OBJECT_TYPE,PR_RECIPIENT_TYPE, PR_DISPLAY_TYPE, PR_DISPLAY_TYPE_EX, PR_ENTRYID));
+
+				$senderStructure = array();
+				$senderStructure["props"]['entryid']         = bin2hex($userprops[PR_ENTRYID]);
+				$senderStructure["props"]['display_name']    = isset($userprops[PR_DISPLAY_NAME]) ? $userprops[PR_DISPLAY_NAME] : '';
+				$senderStructure["props"]['email_address']   = isset($userprops[PR_EMAIL_ADDRESS]) ? $userprops[PR_EMAIL_ADDRESS] : '';
+				$senderStructure["props"]['smtp_address']    = isset($userprops[PR_SMTP_ADDRESS]) ? $userprops[PR_SMTP_ADDRESS] : '';
+				$senderStructure["props"]['address_type']    = isset($userprops[PR_ADDRTYPE]) ? $userprops[PR_ADDRTYPE] : '';
+				$senderStructure["props"]['object_type']     = $userprops[PR_OBJECT_TYPE];
+				$senderStructure["props"]['recipient_type']  = MAPI_TO;
+				$senderStructure["props"]['display_type']    = isset($userprops[PR_DISPLAY_TYPE])    ? $userprops[PR_DISPLAY_TYPE]    : MAPI_MAILUSER;
+				$senderStructure["props"]['display_type_ex'] = isset($userprops[PR_DISPLAY_TYPE_EX]) ? $userprops[PR_DISPLAY_TYPE_EX] : MAPI_MAILUSER;
+			}
+			return $senderStructure;
+		} else {
+			return $GLOBALS["operations"]->getSenderAddress($message);
+		}
 	}
 }
 ?>
